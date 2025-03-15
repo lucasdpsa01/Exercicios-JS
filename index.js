7147640e67c9714ac2a1e31957fe928e74167a25
lucas.dpsa01 <lucasdsa2801@gmail.com>
2025-03-15 20:32:32 -0300

    mais 1

--- index.js
+++ index.js
@@ -64,3 +64,69 @@ console.log(produto.preco);  // 100
 produto.preco = -20;  // "Preço inválido!"
 console.log(produto.preco);  // 100 (não foi alterado)
 
+// Exercício - Tudo
+
+class Banco{
+    static nomeBanco = "Banco do paivinha"
+}
+
+class Conta{
+    
+    #saldo;
+    constructor() {
+        this.#saldo = 0
+    }
+
+    depositar(valor) {
+       if (valor > 0) {
+        this.#saldo += valor
+        console.log("depositado com sucesso!!")
+       } else {
+        console.log("valor inválido")
+       }
+    }
+
+    sacar(valor) {
+        if (valor > this.#saldo) {
+            console.log("Saque inválido")
+        } else {
+            this.#saldo -= valor
+        }
+    }
+
+    get saldo() {
+        return this.#saldo;
+    }
+}
+
+class ContaPoupanca extends Conta{
+    rendimento() {
+        const saldo = this.saldo;
+        const rendimento = saldo * 0.05;
+        this.depositar(rendimento);
+    }
+}
+
+class ContaCorrente extends Conta{
+    rendimento() {
+        const saldo = this.saldo;
+        const rendimento = saldo / 0.05;
+        this.depositar(rendimento);
+    }
+
+    cobrarTaxa() {
+        this.sacar(this.saldo * 0.02);
+    }
+}
+
+console.log(Banco.nomeBanco);
+
+const contaP = new ContaPoupanca();
+contaP.depositar(1000);
+contaP.rendimento();
+console.log(contaP.saldo);  // 1050
+
+const contaC = new ContaCorrente();
+contaC.depositar(1000);
+contaC.cobrarTaxa();
+console.log(contaC.saldo);  // 980
